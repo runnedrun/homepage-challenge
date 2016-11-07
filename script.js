1a12ab79282212068751e2e7b1f0103b6d370f2e
David Gaynor <runnedrun@gmail.com>
2016-11-07 09:32:29 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function getNotesForName(name) {
   })
 }
 
-// objects
-addListItem("product-list", {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"})
-
-
 var functionString = getNotesForName.toString();
 
 var consoleLogExample = $("#ex1");
@@ -19,4 +15,9 @@ var formattedExample = $("<pre>");
 formattedExample.text(functionString);
 
 consoleLogExample.html(formattedExample);
+// objects
+donut = {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"}
+
+
+
 
