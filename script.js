9373b137fb304de7c1e5b2e794f4fd375111cac9
David Gaynor <runnedrun@gmail.com>
2016-11-07 09:43:39 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ consoleLogExample.html(formattedExample);
 
 // objects
 donut = {name: "donut", 
-         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"}
+         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
+         price: "5", 
+         rating: "10/10", 
+         weight: "0.5lb"}
 
 var functionString1 = JSON.stringify(donut)
 
