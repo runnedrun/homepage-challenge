73e7b00e4a3c43cb55b2c9c88fa844e589e94225
David Gaynor <runnedrun@gmail.com>
2016-11-07 09:32:59 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -15,8 +15,20 @@ var formattedExample = $("<pre>");
 formattedExample.text(functionString);
 
 consoleLogExample.html(formattedExample);
+
+
 // objects
 donut = {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"}
+
+var functionString = donut.toString();
+
+var consoleLogExample = $("#ex2");
+
+var formattedExample = $("<pre>");
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
 
 
 
