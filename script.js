751c7c2e81365f760a80325dec60be2334ce09af
David Gaynor <runnedrun@gmail.com>
2016-11-07 09:29:33 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function getNotesForName(name) {
   })
 }
 
+// objects
+addListItem("product-list", {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"})
+
 
 var functionString = getNotesForName.toString();
 
