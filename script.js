4dc37d25ebf4c02609fa427ebd31ed5fa2569a9e
David Gaynor <runnedrun@gmail.com>
2016-11-07 09:44:27 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -34,6 +34,22 @@ formattedExample1.text(functionString1);
 
 consoleLogExample1.html(formattedExample1);
 
+// functions
+donut = {name: "donut", 
+         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
+         price: "5", 
+         rating: "10/10", 
+         weight: "0.5lb"}
+
+var functionString1 = JSON.stringify(donut)
+
+var consoleLogExample1 = $("#ex2");
+
+var formattedExample1 = $("<pre>");
+
+formattedExample1.text(functionString1);
+
+consoleLogExample1.html(formattedExample1);
 
 
 
